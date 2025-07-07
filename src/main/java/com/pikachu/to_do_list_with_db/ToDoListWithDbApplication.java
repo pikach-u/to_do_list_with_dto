d6584dda1ecd61bb8d9a6d24be3eaf25912f94ce
pikach-u <ziwon05@naver.com>
2025-07-07 15:26:06 +0900

    first commit

---
+++ src/main/java/com/pikachu/to_do_list_with_db/ToDoListWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.pikachu.to_do_list_with_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListWithDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pikachu/to_do_list_with_db/ToDoListWithDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pikachu.to_do_list_with_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListWithDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
